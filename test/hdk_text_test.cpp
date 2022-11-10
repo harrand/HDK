46a70b1cf59d6a9c06b3680b4bb30a566cb0b1e8
Harrand <harrysjh98@gmail.com>
2022-11-10 02:00:28 +0000

    * hdk_text_test doesn't need to save the imported text data into a var

--- test/hdk_text_test.cpp
+++ test/hdk_text_test.cpp
@@ -1,7 +1,6 @@
 #include "hdk/hdk.hpp"
 #include "hdk/macros.hpp"
 #include "hdk/debug.hpp"
-#include <string_view>
 #include <cstdio>
 
 #include ImportedTextHeader(test_file, txt)
@@ -10,9 +9,8 @@ int main()
 {
 	hdk::initialise();
 	
-	std::string_view file_data = ImportedTextData(test_file, txt);
 	int answer;
-	std::sscanf(file_data.data(), "%d", &answer);
+	std::sscanf(ImportedTextData(test_file, txt).data(), "%d", &answer);
 	hdk::assert(answer == 42, "Imported text failed. Expected %d, got %d", 42, answer);
 
 	hdk::terminate();
