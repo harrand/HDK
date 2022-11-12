79b895133008c8681ed8744f41ade1a0f298221a
Harrand <harrysjh98@gmail.com>
2022-11-12 21:54:35 +0000

    + Added tracy, but unused as of right now. Also fixed up some of the cmake setup for different build configs

--- cmake/configure.cmake
+++ cmake/configure.cmake
@@ -15,3 +15,36 @@ function(hdk_configure_common)
 		CXX_EXTENSIONS OFF
 	)
 endfunction()
+
+function(hdk_configure_debug)
+	cmake_parse_arguments(
+		HDK_CONFIGURE_DEBUG
+		""
+		"TARGET"
+		""
+		${ARGN}
+	)
+
+	if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+		target_compile_options(${HDK_CONFIGURE_DEBUG_TARGET} PUBLIC /Zi)
+	else()
+		target_compile_options(${HDK_CONFIGURE_DEBUG_TARGET} PUBLIC -g -Og)
+	endif()
+
+endfunction()
+
+function(hdk_configure_release)
+	cmake_parse_arguments(
+		HDK_CONFIGURE_RELEASE
+		""
+		"TARGET"
+		""
+		${ARGN}
+	)
+
+	target_compile_options(${HDK_CONFIGURE_RELEASE_TARGET}
+		PRIVATE
+		-O3
+	)
+
+endfunction()
