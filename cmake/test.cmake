dd02462ba0db7cc968e104c542486e063dbcc04d
Harrand <harrysjh98@gmail.com>
2022-11-15 00:38:52 +0000

    + Add ability to specify base test names, so engines can specify their own test functions

--- cmake/test.cmake
+++ cmake/test.cmake
@@ -1,5 +1,54 @@
 include(CMakeParseArguments)
 
+function(hdk_base_add_test_harness)
+	cmake_parse_arguments(
+		HDK_BASE_ADD_TEST_HARNESS
+		""
+		"BASE_NAME"
+		""
+		${ARGN}
+	)
+
+	cmake_language(EVAL CODE
+	"
+	add_custom_target(${HDK_BASE_ADD_TEST_HARNESS_BASE_NAME}test_build)
+	add_custom_target(${HDK_BASE_ADD_TEST_HARNESS_BASE_NAME}test
+		COMMAND \"${CMAKE_CTEST_COMMAND}\" --output-on-failure --schedule-random -R ${HDK_BASE_ADD_TEST_HARNESS_BASE_NAME}
+			WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
+		)
+	")
+	
+endfunction()
+
+function(hdk_base_add_test)
+	cmake_parse_arguments(
+		HDK_BASE_ADD_TEST
+		""
+		"TARGET;BASE_NAME"
+		"SOURCES"
+		${ARGN}
+	)
+
+	cmake_language(EVAL CODE
+	"
+		${HDK_BASE_ADD_TEST_BASE_NAME}_add_executable(
+			TARGET ${HDK_BASE_ADD_TEST_TARGET}
+			SOURCES ${HDK_BASE_ADD_TEST_SOURCES}
+		)
+
+		add_test(
+			NAME ${HDK_BASE_ADD_TEST_TARGET}
+			COMMAND ${HDK_BASE_ADD_TEST_TARGET}
+			WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
+		)
+
+		add_dependencies(${HDK_BASE_ADD_TEST_BASE_NAME}test_build ${HDK_BASE_ADD_TEST_TARGET})
+		add_dependencies(${HDK_BASE_ADD_TEST_BASE_NAME}test ${HDK_BASE_ADD_TEST_TARGET})
+		message(STATUS \"${HDK_BASE_ADD_TEST_BASE_NAME} - Detected test ${HDK_BASE_ADD_TEST_TARGET}\")
+	")
+	
+endfunction()
+
 function(hdk_add_test)
 	cmake_parse_arguments(
 		HDK_ADD_TEST
@@ -9,24 +58,10 @@ function(hdk_add_test)
 		${ARGN}
 	)
 
-	hdk_add_executable(
+	hdk_base_add_test(
 		TARGET ${HDK_ADD_TEST_TARGET}
+		BASE_NAME hdk
 		SOURCES ${HDK_ADD_TEST_SOURCES}
 	)
-
-	add_test(
-		NAME ${HDK_ADD_TEST_TARGET}
-		COMMAND ${HDK_ADD_TEST_TARGET}
-		WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
-	)
-
-	add_dependencies(hdktest_build ${HDK_ADD_TEST_TARGET})
-	add_dependencies(hdktest ${HDK_ADD_TEST_TARGET})
-	message(STATUS "Detected test ${HDK_ADD_TEST_TARGET}")
 endfunction()
-
-add_custom_target(hdktest_build)
-add_custom_target(hdktest
-	COMMAND "${CMAKE_CTEST_COMMAND}" --output-on-failure --schedule-random -C -Debug
-	WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
-)
+hdk_base_add_test_harness(BASE_NAME hdk)
