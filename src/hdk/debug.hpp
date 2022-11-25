a59eb3938493fc1f5bf601c31301d61b1d5a712d
Harrand <harrysjh98@gmail.com>
2022-11-25 02:43:37 +0000

    * Minor documentation pass for debug.hpp and memblk

--- src/hdk/debug.hpp
+++ src/hdk/debug.hpp
@@ -11,6 +11,7 @@ namespace hdk
 {
 	namespace detail
 	{
+		/// C-String wrapper with source-location information secretly present. From a user perspective, assume this is merely a `const char*`.
 		struct format_string
 		{
 			const char* str;
@@ -21,18 +22,35 @@ namespace hdk
 		};
 	}
 
+	/**
+ 	 * @ingroup hdk
+	 * Assert on a condition. If the condition is false, `hdk::error` is invoked. Only works on debug builds (aka `if HDK_DEBUG`).
+	 */
 	template<typename... Args>
 	void assert(bool condition, detail::format_string fmt = "<No message>", Args&&... args);
 
+	/**
+	 * @ingroup hdk
+	 * Breakpoint if a debugger is present. If no debugger is present, terminate the application with the given message. Only works on debug builds (aka `if HDK_DEBUG`).
+	 */ 
 	template<typename... Args>
 	void error(detail::format_string fmt = "<No message>", Args&&... args);
 
+	/**
+	 * @ingroup hdk
+	 * Print out the formatted message to standard output, including source location information. Only works on debug builds.
+	 */ 
 	template<typename... Args>
 	void report(detail::format_string fmt, Args&&... args);
 
 	void debug_break();
 }
 
+/**
+ * Legacy assert. Provides similar functionality to `hdk::assert`, although the calling notation directly matches `assert` from <cassert>.
+ *
+ * Some libraries allow you to define your own assert implementation. If so, it is recommended to define it to hdk_legacy_assert if you want to be able to debug-break on assertions. Note that this is entirely optional, no HDK behaviour requires this so exist.
+ */
 #define hdk_legacy_assert(cond) hdk::assert(cond, "Legacy Assert Detected")
 
 #include "hdk/debug.inl"
--- src/hdk/memory/memblk.hpp
+++ src/hdk/memory/memblk.hpp
@@ -4,13 +4,20 @@
 
 namespace hdk
 {
+	/**
+	* @ingroup hdk
+	* A non-owning, contiguous block of memory.
+	*/
 	struct memblk
 	{
+		/// Start address of the block.
 		void* ptr;
+		/// Size of the block, in bytes.
 		std::size_t size;
 		
 		bool operator==(const memblk& rhs) const = default;
 	};
+	/// Represents the null block.
 	constexpr memblk nullblk{.ptr = nullptr, .size = 0};
 }
 
