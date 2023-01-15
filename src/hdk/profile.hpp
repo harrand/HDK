b36a7b101deea946172a15888b07ca509ba05d15
Harrand <harrysjh98@gmail.com>
2023-01-15 17:32:51 +0000

    * Rename UNIQUE_NAME to HDK_UNIQUE_NAME as funnily enough the former clashes with a gcc core header

--- src/hdk/profile.hpp
+++ src/hdk/profile.hpp
@@ -11,8 +11,8 @@
 	#define CONCAT_HDKPROF(a, b) CONCAT_INNER_HDKPROF(a, b)
 	#define CONCAT_INNER_HDKPROF(a, b) a ## b
 
-	#define UNIQUE_NAME(base) CONCAT_HDKPROF(base, __LINE__)
-	#define HDK_PROFZONE(name, colour) ZoneNamedNC(UNIQUE_NAME(tracy_profvar), name, colour, true)
+	#define HDK_UNIQUE_NAME(base) CONCAT_HDKPROF(base, __LINE__)
+	#define HDK_PROFZONE(name, colour) ZoneNamedNC(HDK_UNIQUE_NAME(tracy_profvar), name, colour, true)
 	#define HDK_THREAD(name) tracy::SetThreadName(name)
 	#define HDK_FRAME FrameMark
 	#define HDK_FRAME_BEGIN FrameMarkStart("Frame Loop")
